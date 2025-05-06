f1e3325a20f5931bdc2d4be9cf31678b0d9ea576
rodrigo-lombezzi <rodrigoylombezzi@gmail.com>
2025-05-06 08:19:17 -0300

    :iphone: Footer e header respinsiveis

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,45 +1,40 @@
-import { useNavigate} from 'react-router-dom';
-import { routes } from "../../routes/routes";
-import Logo from "../../assets/images/logo.png";
+import { useNavigate } from 'react-router-dom'
+import { routes } from '../../routes/routes'
+import Logo from '../../assets/images/logo.png'
 
 export default function Header() {
-  const navigate = useNavigate();
-
-  const handleLoginClick = () => {
-    navigate(routes.LOGIN);
-  };
+  const navigate = useNavigate()
+  const handleLoginClick = () => navigate(routes.LOGIN)
 
   return (
-    <header className="w-full h-[58px] bg-primary text-textPrimary shadow-md fixed ">
-    <div className="flex items-center justify-between h-full px-4 space-x-[15px] mr-8">
-
-      <div className="flex items-center h-full ml-8 rounded">
-        <img src={Logo} alt="Logo" className="h-11 rounded" />
+    <header className="w-full bg-primary text-textPrimary shadow-md fixed top-0 z-50">
+      <div className="container mx-auto flex items-center justify-between h-[58px] px-4 md:px-2">
+        <div className="order-1">
+          <img src={Logo} alt="Logo" className="h-10 md:h-11 rounded"/>
+        </div>
+        <div className="flex items-center space-x-2 md:space-x-4 order-2">
+          <nav className="hidden md:flex space-x-4 text-sm font-normal">
+            <a href="#" className="hover:text-textSecondary">Home</a>
+            <a href="#" className="hover:text-textSecondary">Our Story</a>
+            <a href="#" className="hover:text-textSecondary">Our Team</a>
+          </nav>
+          <button
+            onClick={handleLoginClick}
+            className="hidden md:inline-block bg-surface px-3 py-1.5 border border-textSecondary text-textSecondary rounded text-sm transition hover:bg-neutralLighter"
+          >
+            Access Account
+          </button>
+          <button
+            onClick={() => {}}
+            className="hidden md:inline-block bg-secondary text-white px-3 py-1.5 rounded text-sm transition hover:bg-hoverSuccess hover:text-textSecondary"
+          >
+            Join Us
+          </button>
+          <button className="md:hidden text-xl leading-none">
+            ☰
+          </button>
+        </div>
       </div>
-
-      <div className="flex items-center gap-5 h-full">
-      <nav className="flex space-x-4 text-textPrimary font-normal text-sm">
-          <a href="#" className="hover:text-textSecondary ">Home</a>
-          <a href="#" className="hover:text-textSecondary">Our Story</a>
-          <a href="#" className="hover:text-textSecondary">Our Team</a>
-        </nav>
-
-        <button
-          onClick={handleLoginClick}
-          className="bg-surface px-3 py-1.5 border border-textSecondary text-textSecondary rounded transition hover:bg-neutralLighter text-sm"
-        >
-          Access Account
-        </button>
-        <button
-          onClick={() => console.log('Join Us clicked')}
-          className="bg-secondary text-white hover:bg-hoverSuccess px-3 py-1.5 rounded transition hover:text-textSecondary text-sm"
-        >
-          Join Us
-        </button>
-      </div>
-
-    </div>
-  </header>
-
-);
+    </header>
+  )
 }
