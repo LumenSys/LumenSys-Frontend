580cd524cec967ddced05dd786b345e09322e355
FrancianeRamos <francianeramso649@gmail.com>
2025-05-08 14:31:39 -0300

    :sparkles: feat: Add Input component with label and styling

---
+++ frontend/src/components/Input/index.tsx
@@ -0,0 +1,15 @@
+const Input = () => (
+    <>
+      <label className="mb-[10px] block text-base font-medium text-dark dark:text-white">
+        Input
+      </label>
+      <input
+        type="text"
+        placeholder="Default Input"
+        className="w-full bg-transparent rounded-md border border-stroke dark:border-dark-3 py-[10px] px-5 text-dark-6 outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2"
+      />
+    </>
+  );
+  
+  export default Input;
+  
