dad6d461ee07dbbe2817e43b948971c955dabfe1
FrancianeRamos <francianeramos649@gmail.com>
2025-06-19 10:44:22 -0300

    :sparkles: feat: Implementar página Gerenciar Planos com layout e elementos visuais

---
+++ frontend/src/pages/GerenciarPlanos/index.tsx
@@ -0,0 +1,67 @@
+export default function GerenciarPlanos() {
+  return (
+<div className="w-[1920px] h-[1080px] relative bg-neutral-50 overflow-hidden">
+  <div className="w-[2418px] px-2 left-[-498px] top-0 absolute bg-sky-300 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] inline-flex justify-start items-start">
+    <div className="w-[1775px] h-16 bg-sky-300 flex justify-end items-center gap-4">
+      <div className="w-[1244px] h-16 flex justify-start items-center gap-2.5 flex-wrap content-center">
+        <img className="w-16 h-16 rounded" src="https://placehold.co/68x68" />
+      </div>
+    </div>
+  </div>
+  <div className="w-[1726px] h-[727px] left-[94px] top-[206px] absolute bg-zinc-400 rounded-lg shadow-[0px_6px_10px_0px_rgba(0,0,0,0.10)]">
+    <div className="w-[1662px] h-72 left-[32px] top-[41px] absolute">
+      <div className="w-4 h-4 left-[12px] top-[44px] absolute" />
+      <div className="w-28 h-4 left-[229.98px] top-[100px] absolute" />
+      <div className="w-4 h-4 left-[12px] top-[138px] absolute" />
+      <div className="w-96 h-4 left-[105px] top-[36px] absolute" />
+    </div>
+    <div className="w-72 h-10 left-[1304px] top-[20px] absolute bg-white rounded-md outline outline-1 outline-offset-[-1px] outline-slate-200 overflow-hidden">
+      <div className="w-96 h-4 left-[-15px] top-[12px] absolute overflow-hidden">
+        <div className="left-[28px] top-[0.50px] absolute justify-start text-slate-500 text-sm font-normal font-['Inter']">Buscar por ID</div>
+      </div>
+      <img className="w-8 h-8 left-[242px] top-[5px] absolute" src="https://placehold.co/31x31" />
+    </div>
+  </div>
+  <div className="w-28 h-3.5 left-[8px] top-[-76px] absolute text-center justify-start text-gray-700 text-xs font-normal font-['Inter'] leading-tight">Contate o suporte</div>
+  <div className="w-80 h-9 left-[798px] top-[229px] absolute text-center justify-start text-gray-800 text-3xl font-bold font-['Inter'] leading-9">Planos<br/></div>
+  <div className="w-[1645px] h-96 left-[138px] top-[300px] absolute">
+    <div className="w-14 h-5 left-0 top-0 absolute justify-start text-gray-700 text-base font-normal font-['Inter'] leading-tight">Email</div>
+    <div className="w-14 h-5 left-[1210px] top-0 absolute justify-start text-gray-700 text-base font-normal font-['Inter'] leading-tight">Status</div>
+    <div className="w-14 h-5 left-[1326px] top-0 absolute justify-start text-gray-700 text-base font-normal font-['Inter'] leading-tight">Editar</div>
+    <div className="w-[1645px] h-10 left-0 top-[28px] absolute bg-white rounded-md outline outline-1 outline-offset-[-1px] outline-slate-200 overflow-hidden">
+      <div className="w-96 h-4 left-[41px] top-[12.50px] absolute overflow-hidden">
+        <div className="w-24 h-3.5 left-0 top-[1px] absolute justify-start text-slate-500 text-sm font-normal font-['Inter']">seu@email.com</div>
+      </div>
+      <div className="w-10 h-5 left-[1209px] top-[10px] absolute justify-start text-gray-700 text-base font-normal font-['Inter'] leading-tight">Ativo </div>
+      <img className="w-12 h-10 left-[1322px] top-0 absolute" src="https://placehold.co/50x40" />
+      <img className="w-7 h-5 left-[1248px] top-[10px] absolute" src="https://placehold.co/30x20" />
+    </div>
+    <div className="w-36 h-5 left-0 top-[94px] absolute justify-start text-gray-700 text-base font-normal font-['Inter'] leading-tight">Nome de usuário</div>
+    <div className="w-36 h-5 left-0 top-[183px] absolute justify-start text-gray-700 text-base font-normal font-['Inter'] leading-tight">Senha</div>
+    <div className="w-28 h-4 left-[393.98px] top-[115px] absolute" />
+    <div className="w-[1645px] h-10 left-0 top-[122px] absolute bg-white rounded-md outline outline-1 outline-offset-[-1px] outline-slate-200 overflow-hidden">
+      <div className="w-96 h-4 left-[41px] top-[12.50px] absolute" />
+      <div className="w-96 h-4 left-[41px] top-[12.50px] absolute overflow-hidden">
+        <div className="left-0 top-[0.50px] absolute justify-start text-slate-500 text-sm font-normal font-['Inter']">Nome usuário </div>
+      </div>
+    </div>
+    <div className="w-60 h-12 left-0 top-[549px] absolute bg-sky-500 rounded-md">
+      <div className="w-40 h-7 left-[38px] top-[14px] absolute text-center justify-start text-white text-sm font-normal font-['Inter'] leading-tight">Cadastrar novo plano</div>
+    </div>
+    <div className="w-[1645px] h-10 left-0 top-[211px] absolute bg-white rounded-md outline outline-1 outline-offset-[-1px] outline-slate-200 overflow-hidden">
+      <div className="w-96 h-4 left-[41px] top-[12.50px] absolute overflow-hidden">
+        <div className="w-7 h-3.5 left-0 top-[1px] absolute justify-start text-slate-500 text-[10.28px] font-normal font-['Inter']">••••••</div>
+      </div>
+      <div className="w-96 h-4 left-[41px] top-[12.50px] absolute" />
+    </div>
+    <div className="w-36 h-5 left-0 top-[277px] absolute justify-start text-gray-700 text-base font-normal font-['Inter'] leading-tight">Confirmar senha</div>
+    <div className="w-[1645px] h-10 left-0 top-[305px] absolute bg-white rounded-md outline outline-1 outline-offset-[-1px] outline-slate-200 overflow-hidden">
+      <div className="w-96 h-4 left-[41px] top-[12.50px] absolute overflow-hidden">
+        <div className="w-7 h-3.5 left-0 top-[1px] absolute justify-start text-slate-500 text-[10.28px] font-normal font-['Inter']">••••••</div>
+      </div>
+      <div className="w-96 h-4 left-[41px] top-[12.50px] absolute" />
+    </div>
+  </div>
+</div>
+    );
+    }
