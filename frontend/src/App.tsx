5abe76b79485bfb5828f0c5ae3977bce08d2327b
FrancianeRamos <francianeramos649@gmail.com>
2025-08-19 15:31:44 -0300

    :sparkles: feat: Implement CompanyContext for global state management of companies, including add, update, delete, and retrieval functionalities

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,25 @@
-import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CompanyProvider } from './context/CompanyContext';
+import ListOfCompanies from './pages/Listofcompanies';
+import CadastroEmpresa from './pages/CadastroEmpresa';
+import Sidebar from './components/Sidebar';
 
-export default function App() {
+function App() {
   return (
-    <AppRoutes />
+    <CompanyProvider>
+      <Router>
+        <div className="flex">
+          <Sidebar />
+          <main className="ml-64 flex-1 min-h-screen">
+            <Routes>
+              <Route path="/listaempresas" element={<ListOfCompanies />} />
+              <Route path="/CadastroEmpresa" element={<CadastroEmpresa />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </CompanyProvider>
   );
 }
 
+export default App;
